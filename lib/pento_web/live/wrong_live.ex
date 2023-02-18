d1248c0f1ccf5c85cce829fc3111c4905d958e9e
Astha Jada <astha.jada@gmail.com>
2023-02-18 12:02:45 +0900

    add live_session

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -1,8 +1,14 @@
 defmodule PentoWeb.WrongLive do
+  alias Pento.Accounts
   use Phoenix.LiveView, layout: {PentoWeb.LayoutView, "live.html"}
 
-  def mount(_params, _session, socket) do
-    {:ok, assign(socket, score: 0, message: "Make a guess:")}
+  def mount(_params, session, socket) do
+    {:ok,
+     assign(socket,
+       score: 0,
+       message: "Make a guess:",
+       session_id: session["live_socket_id"]
+     )}
   end
 
   def render(assigns) do
@@ -15,6 +21,10 @@ defmodule PentoWeb.WrongLive do
       <%= for n <- 1..10 do %>
         <a href="#" phx-click="guess" phx-value-number={n}><%= n %></a>
       <% end %>
+      <pre>
+        <%= @current_user.email %>
+        <%= @session_id %>
+      </pre>
     </h2>
     """
   end
